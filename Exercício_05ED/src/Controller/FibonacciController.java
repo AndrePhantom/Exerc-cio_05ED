b29b1ac3adceeee7e9062a0d74a19c7007c6781c
perkles <andre_cassara@yahoo.com.br>
2022-08-25 16:58:54 -0300

    primeiro commit

---
+++ Exercício_05ED/src/Controller/FibonacciController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+public class FibonacciController {
+	public class Fibonacci {
+
+	    public static long fibo(int n) {
+	        if (n < 2) {
+	            return n;
+	        } else {
+	            return fibo(n - 1) + fibo(n - 2);
+	        }
+	    }
+	}
+}
---
+++ Exercício_05ED/src/view/Main.java
@@ -0,0 +1,17 @@
+package view;
+
+import Controller.FibonacciController.Fibonacci;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// teste do programa. Imprime os 20 primeiros termos
+		for (int i = 0; i < 20; i++) {
+	            System.out.print("(" + i + "):" + Fibonacci.fibo(i) + "\t");
+	        }
+
+	    }
+
+	}
+
+	
